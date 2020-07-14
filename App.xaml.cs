8f54975e48432e76437704ffab921d15feaa0b27
Il Harper <afanyiyu@hotmail.com>
2020-07-15 00:24:50 +0800

    feat: App Startup Logic Update

--- App.xaml.cs
+++ App.xaml.cs
@@ -2,9 +2,13 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using MetroRadiance.UI;
+using Ruminoid.Dashboard.Windows;
 
 namespace Ruminoid.Dashboard
 {
@@ -13,5 +17,47 @@ namespace Ruminoid.Dashboard
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            DispatcherUnhandledException += (sender, args) =>
+            {
+                args.Handled = true;
+                if (args.Exception.Source.Contains("VLC") || args.Exception.Message.Contains("VideoView")) return;
+                MessageBox.Show(
+                    args.Exception.Message,
+                    "灾难性故障",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Error,
+                    MessageBoxResult.OK);
+            };
+
+            AppDomain.CurrentDomain.UnhandledException += (sender, args) =>
+            {
+                if (((Exception)args.ExceptionObject).Source.Contains("VLC") ||
+                    ((Exception)args.ExceptionObject).Message.Contains("VideoView")) return;
+                MessageBox.Show(
+                    ((Exception)args.ExceptionObject)?.Message ?? "Exception",
+                    "灾难性故障",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Error,
+                    MessageBoxResult.OK);
+            };
+
+            Dashboard.Properties.Resources.Culture = CultureInfo.CurrentUICulture;
+
+            if (MainWindow is null) MainWindow = new MainWindow();
+            MainWindow.Show();
+
+            Current.Dispatcher?.Invoke(() => ThemeService.Current.ChangeTheme(Theme.Dark));
+            Current.Dispatcher?.Invoke(() => ThemeService.Current.ChangeAccent(Accent.Blue));
+
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            ThemeService.Current.Register(this, Theme.Windows, Accent.Windows);
+        }
     }
 }
