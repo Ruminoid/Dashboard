c262742b6ffbf2853b7c4084c83c82e810967a13
Il Harper <afanyiyu@hotmail.com>
2020-07-17 12:43:15 +0800

    feat: Add MainWindow DataContext

---
+++ Windows/MainWindowDataContext.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ruminoid.Dashboard.Windows
+{
+    public partial class MainWindow : INotifyPropertyChanged
+    {
+        #region DataContext
+
+        private string _appVersion = "";
+
+        public string AppVersion
+        {
+            get => _appVersion;
+            set
+            {
+                _appVersion = value;
+                OnPropertyChanged();
+            }
+        }
+
+        #endregion
+
+        #region PropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+    }
+}
